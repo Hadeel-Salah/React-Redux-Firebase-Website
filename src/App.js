d920a95d7916d0fcb8a8d9a036dcbdbedf9317f8
hadeel salah <hadeelsalah80@gmail.com>
2020-09-13 15:16:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {BrowserRouter} from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
 import './App.css';
-
+import Navbar from './components/layout/Navbar/Navbar'
 function App() {
   return (
       <div className="App">
-        <h1>g</h1>
+        <BrowserRouter>
+        <Navbar />
+        </BrowserRouter>
       </div>
   );
 }
