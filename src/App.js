b1c9333c547a35e53016e13f7b01e4f55057ed55
hadeel salah <hadeelsalah80@gmail.com>
2020-09-13 23:00:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import Navbar from './components/layout/Navbar/Navbar'
 import Dashboard from './components/Dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails/ProjectDetails'
-
+import SignIn from './components/auth/SignIn/SignIn';
+import SignUp from './components/auth/SignUp/SignUp';
 function App() {
   return (
       <div className="App">
@@ -13,7 +14,8 @@ function App() {
         <Switch>
             <Route exact path='/'component={Dashboard} />
             <Route path='/project/:id' component={ProjectDetails} />
-
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
           </Switch> 
         </BrowserRouter>
       </div>
