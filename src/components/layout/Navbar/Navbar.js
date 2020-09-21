fcea32f9876a55b47f9c21387f90323fd5b05e84
hadeel salah <hadeelsalah80@gmail.com>
2020-09-21 15:42:43 +0300

    Update Navbar.js

--- src/components/layout/Navbar/Navbar.js
+++ src/components/layout/Navbar/Navbar.js
@@ -2,21 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedOutLinks from '../SignOut/SignOutLinks';
 import SignedInLinks from '../SignIn/SignInLinks';
+import { connect } from 'react-redux'
 
-const Navbar = () =>{
+
+const Navbar = (props) =>{
+    const { auth, profile } = props;
+    const links = auth.uid ? <SignedInLinks profile={profile} /> : <SignedOutLinks />;
     return(
         <nav>
             <div className="nav-weapper grey dartken-3" style={{position: 'relative'}}>
-                <Link to='/' 
-                      className="brand-logo" 
-                      style={{position: 'absolute', left:'30px'}}
+                <Link 
+                    to='/' 
+                    className="brand-logo" 
+                    style={{position: 'absolute', left:'30px'}}
 
                 >BrandProject</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+             {links}
        </div>
        </nav>
 
     )
 }
-export default Navbar;
+const mapStateToProps = (state) => {
+    return{
+      auth: state.firebase.auth,
+      profile: state.firebase.profile
+    }
+  }
+  
+  export default connect(mapStateToProps)(Navbar)
