1a2870d44a2ce325cc587ea039da227e4e8d8f97
hadeel salah <hadeelsalah80@gmail.com>
2020-09-21 15:58:12 +0300

    update authorFirstName and lastName and ID from firebase

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,11 +1,15 @@
 export const createProject = (project) => {
   return (dispatch, getState, { getFirestore}) => {
+
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
+    
     firestore.collection('projects').add({
       ...project,
-      authorFirstName: 'hadeel',
-      authorLastName: 'salah',
-      authorId: 123456,
+      authorFirstName: profile.firstName,
+      authorLastName: profile.lastName,
+      authorId: authorId,
       createdAt: new Date()
     }).then(() => {
       dispatch({ type: 'CREATE_PROJECT_SUCCESS' });
