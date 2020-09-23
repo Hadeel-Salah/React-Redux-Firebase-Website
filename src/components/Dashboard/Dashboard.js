903ca10fe1ad5c98a5d4f7c9f1214fe8119622d9
hadeel salah <hadeelsalah80@gmail.com>
2020-09-23 20:57:42 +0300

    passing props to Notifications

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react'
 import ProjectList from '../projects/ProjectList/ProjectList'
 import Notifications from './Notifications/Notifications'
 import { connect } from 'react-redux'
-import { firestoreConnect } from 'react-redux-firebase';
+import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
+import { Redirect } from 'react-router-dom'
+
+
 
 class Dashboard extends Component {
   render() {
+    const { projects, auth, notifications } = this.props;
+    if (!auth.uid) return <Redirect to='/signin' /> 
 
-    const { projects } = this.props;
-    
     return (
       <div className="dashboard container">
         <div className="row">
@@ -17,7 +20,7 @@ class Dashboard extends Component {
             <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
-            <Notifications />
+            <Notifications notifications={notifications} />
           </div>
         </div>
       </div>
@@ -26,13 +29,17 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
-  // console.log(state);
   return {
-    projects: state.firestore.ordered.projects
+    projects: state.firestore.ordered.projects,
+    auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications
   }
 }
 
 export default compose(
-  firestoreConnect(() => ['projects']),
-  connect(mapStateToProps)
-  )(Dashboard)
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'projects', orderBy: ['createdAt', 'desc']},
+    { collection: 'notifications', limit: 3, orderBy: ['time', 'desc']}
+  ])
+)(Dashboard)
